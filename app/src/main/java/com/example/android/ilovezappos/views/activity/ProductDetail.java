aed3899f9e6afd122777c09959ee0a2f0154ba99
jennifernghinguyen <jennifernghi@yahoo.com>
2017-02-06 23:16:09 -0800

    home button

--- app/src/main/java/com/example/android/ilovezappos/views/activity/ProductDetail.java
+++ app/src/main/java/com/example/android/ilovezappos/views/activity/ProductDetail.java
@@ -3,8 +3,11 @@ package com.example.android.ilovezappos.views.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.android.ilovezappos.Controller.ProductDetailController;
 import com.example.android.ilovezappos.R;
@@ -22,18 +25,33 @@ public class ProductDetail extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_product_detail);
         getProductInfo();
+        showHomeArrow();
     }
 
     public static void startIntent(Context context, Product product){
         Intent intent = new Intent(context, ProductDetail.class);
         intent.putExtra(PRODUCT_INFO, product);
         context.startActivity(intent);
-        //return intent;
     }
     private void getProductInfo() {
         Product people = (Product) getIntent().getSerializableExtra(PRODUCT_INFO);
         ProductDetailController productDetailController = new ProductDetailController(this,people);
         binding.setProductDetailController(productDetailController);
         setTitle(people.getProductName());
+    }
+
+    private void showHomeArrow() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
